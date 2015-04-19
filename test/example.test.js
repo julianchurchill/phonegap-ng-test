0e58289fe7c5809f5af6d0a6ca0c2736688c35db
Julian Churchill <chewielouie03@gmail.com>
2015-04-19 21:09:38 +0100

    Added a running example jasmine test

--- test/example.test.js
+++ test/example.test.js
@@ -1,41 +1,11 @@
-// if (typeof NS == 'undefined') { NS = {}; }
+describe("myFunction", function() {
+    beforeEach(function(){
+    });
 
-// NS.myFunction = {
-//     //empty stuff array, filled during initialization
-//     stuff: [],
+    afterEach(function() {
+    });
 
-//     init: function init() {
-//         this.stuff.push('Testing');
-//     },
-//     reset: function reset() {
-//         this.stuff = [];
-//     },
-//     //will add new functionality here later
-// };
-
-// NS.myFunction.init();
-
-// describe("myFunction", function() {
-//     var myfunc = NS.myFunction;
-
-//     beforeEach(function(){
-//         spyOn(myfunc, 'init').andCallThrough();
-//     });
-
-//     afterEach(function() {
-//         myfunc.reset();
-//     });
-
-//     it("should be able to initialize", function() {
-//         expect(myfunc.init).toBeDefined();
-//         myfunc.init();
-//         expect(myfunc.init).toHaveBeenCalled();
-//     });
-
-//     it("should populate stuff during initialization", function(){
-//         myfunc.init();
-//         expect(myfunc.stuff.length).toEqual(1);
-//         expect(myfunc.stuff[0]).toEqual('Testing');
-//     });
-//     //will insert additional tests here later
-// });
+    it("test the jasmine unit test framework", function() {
+        expect(true).toBe(true);
+    });
+});
