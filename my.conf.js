e03c93b65435b4dd0d9aade7f48ebaee3b719d75
Julian Churchill <chewielouie03@gmail.com>
2015-04-19 21:02:27 +0100

    Added empty test file and karma test configuration

--- my.conf.js
+++ my.conf.js
@@ -10,11 +10,12 @@ module.exports = function(config) {
 
     // frameworks to use
     // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
+    frameworks: ['jasmine', 'browserify'],
 
 
     // list of files / patterns to load in the browser
     files: [
+      'bower_components/angular/angular.js',
       'src/**/*.js',
       'test/**/*.js'
     ],
@@ -28,8 +29,13 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'test/*': ['browserify']
     },
 
+    browserify: {
+        watch: true,
+        debug: true
+    },
 
     // test results reporter to use
     // possible values: 'dots', 'progress'
@@ -55,8 +61,9 @@ module.exports = function(config) {
 
 
     // start these browsers
+    // Available: Chrome, PhantomJS
     // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
-    browsers: ['Chrome'],
+    browsers: ['PhantomJS'],
 
 
     // Continuous Integration mode
